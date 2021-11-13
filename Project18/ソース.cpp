e21bc86292d78752397eda5c1a14cf026678fdc2
ZamaVerce2 <Verce@LAPTOP-8JD0V3Q5>
2021-11-13 19:33:08 +0900

    プロジェクト ファイルを追加します。

---
+++ Project18/ソース.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int student[5];
+	int sum;
+	double avg;
+	int i;
+
+	student[0] = 62;
+	student[1] = 56;
+	student[2] = 82;
+	student[3] = 77;
+	student[4] = 90;
+
+	sum = 0; 
+	for ( i = 0; i < 5; i++)
+	{
+		sum += student[i];
+	}
+
+	avg = sum / 5.0;
+	cout << "•½‹Ï“_‚Í" << avg << "“_‚Å‚·B\n";
+
+	return 0;
+}
